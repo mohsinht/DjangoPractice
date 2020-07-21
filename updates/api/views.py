b1c29015581c794ed3be6c669c2fd3f45db6b001
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-07-21 18:35:30 +0500

    Add practical API skeleton

---
+++ updates/api/views.py
@@ -0,0 +1,26 @@
+import json
+from updates.models import Update as UpdateModel
+
+
+class UpdateModelDetailAPIView():
+    # Retrieve:
+    def get(self, request, *args, **kwargs):
+        return  # json
+
+    # Create:
+    def post(self, request, *args, **kwargs):
+        return  # json
+
+    # Update:
+    def put(self, request, *args, **kwargs):
+        return  # json
+
+    # Remove:
+    def delete(self, request, *args, **kwargs):
+        return  # json
+
+
+class UpdateModelListAPIView():
+    # Retrieve:
+    def get(self, request, *args, **kwargs):
+        return  # json
