303d80f4dd09d8a8ea0826f21546227016aa2a32
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-07-21 15:10:49 +0500

    Add class based views with example data

--- updates/views.py
+++ updates/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
 
+from cfeapi.mixins import JsonResponseMixin
 from .models import Update
 
 
@@ -16,16 +17,17 @@ def json_example_view(request):
 
 class JsonCBV(View):
     def get(self, request, *args, **kwargs):
-            data = {
+        data = {
             "count": 100,
             "content": "Some New Content"
         }
         return JsonResponse(data)
-    
 
-class JsonResponseMixin(object):
-    def render_to_json_response(self, context, **response_kwargs):
-        return JsonResponse(self.get_data(context), **response_kwargs)
 
-    def get_data(self, context):
-        return context
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *arg, **kwargs):
+        data = {
+            "count": 100,
+            "content": "Some New Content"
+        }
+        return self.render_to_json_response(data)
