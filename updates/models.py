f5a641778a93d318412024569cba5653c7626658
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-07-21 18:08:28 +0500

    Clean code of serialize

--- updates/models.py
+++ updates/models.py
@@ -9,17 +9,9 @@ def upload_update_image(instance, filename):
 
 
 class UpdateQuerySet(models.QuerySet):
-    # def serialize(self):
-    #     qs = self
-    #     return serialize("json", qs, fields=('user', 'content', 'image'))
-
     def serialize(self):
-        qs = self
-        final_array = []
-        for obj in qs:
-            struct = json.loads(obj.serialize())
-            final_array.append(struct)
-        return json.dumps(final_array)
+        list_values = list(self.values("user", "content", "image"))
+        return json.dumps(list_values)
 
 
 class UpdateManager(models.Manager):
@@ -41,10 +33,14 @@ class Update(models.Model):
         return self.content or ""
 
     def serialize(self):
-        print("serializing object!")
-        json_data = serialize(
-            "json", [self], fields=('user', 'content', 'image'))
-        struct = json.loads(json_data)
-        print(struct)
-        data = json.dumps(struct[0]['fields'])
+        try:
+            image = self.image.url
+        except:
+            image = ''
+        data = {
+            "content": self.content,
+            "user": self.user.id,
+            "image": image
+        }
+        data = json.dumps(data)
         return data
